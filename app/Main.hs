b203a0699d39bbd52eaad3a69427e26bc0b49851
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-11-03 17:59:55 -0500

    feat: parse binary, decimal and octal numbers

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment (getArgs)
+import Numeric (readFloat, readHex, readOct, readBin)
+import Data.Maybe (listToMaybe, isNothing, fromJust)
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -15,8 +17,8 @@ parseChar = do
   _ <- char '\''
   x <- letter
   _ <- char '\''
-  return $ Character x  
-  
+  return $ Character x
+
 parseString :: Parser LispVal
 parseString = do
     _ <- char '"'
@@ -35,16 +37,32 @@ parseAtom = do
              _    -> Atom atom
 
 parseNumber :: Parser LispVal
-  -- parseNumber = liftM (Number . read) $ many1 digit
--- liftM :: Monad m => (a1 -> r) -> m a1 -> m r
--- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+-- parseNumber = liftM (Number . read) $ many1 digit
 parseNumber = do
-  many1 digit >>= \x -> return (Number . read  $  x)
+  _ <- oneOf "#"
+  base <- oneOf "bodx"
+  digits <- many1 digit
+  return $ Number (val base digits)
+    where
+      val :: Char -> String -> Integer
+      val base digits = output
+        where
+          output :: Integer
+          output
+                | Just (x,_) <- maybeOutput = x
+                | otherwise = error "Invalid number format"
+                where
+                  maybeOutput :: Maybe (Integer, String)
+                  maybeOutput = 
+                        case base of  
+                            'o' ->  listToMaybe $ readOct digits
+                            _ ->  read digits
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
             <|> parseString
             <|> parseNumber
+
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
@@ -67,3 +85,4 @@ main = do
   (expr:_) <- getArgs
   putStrLn (readExpr expr)
 
+  
