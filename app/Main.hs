c78dc1821a3cad3f9056398da705c8d9194c00fe
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-11-04 14:24:12 -0500

    feat: add error handling

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment (getArgs)
-import Control.Monad (liftM)
+import Control.Monad.Except
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -147,6 +147,26 @@ eval (List (Atom func: args)) = apply func $ map eval args
 
 
 instance Show LispVal where show = showVal
+
+data LispError = NumArgs Integer [LispVal]
+                 | TypeMismatch String LispVal
+                 | Parser ParseError
+                 | BadSpecialForm String LispVal
+                 | NotFunction String String
+                 | UnboundVar String String
+                 | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ func ++ " not found"
+showError (NumArgs expected found) = "Expected " ++ show expected ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found " ++ show found
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
+
+instance Show LispError where show = showError
+
+type ThrowsError = Either LispError -- partial type
 
 data LispVal =
   Atom String
